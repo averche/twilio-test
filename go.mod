cee58914e1add88ae593a8e1f1f80e1d3dd000d7
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2024-09-24 13:52:31 -0400

    Clean up Twilio API keys

---
+++ go.mod
@@ -0,0 +1,18 @@
+module twilio-test
+
+go 1.22.3
+
+toolchain go1.22.4
+
+require github.com/twilio/twilio-go v1.21.0
+
+require (
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	twilio "github.com/twilio/twilio-go"
+	twilio_client "github.com/twilio/twilio-go/client"
+	twilio_api "github.com/twilio/twilio-go/rest/api/v2010"
+)
+
+func mustEnv(env string) string {
+	value, ok := os.LookupEnv(env)
+	if !ok {
+		panic(fmt.Sprintf("missing %q environment variable", env))
+	}
+	return value
+}
+
+func main() {
+	var (
+		twilioAccount      = mustEnv("TWILIO_ACCOUNT_SID")
+		twilioAPIKey       = mustEnv("TWILIO_API_KEY_SID")
+		twilioAPIKeySecret = mustEnv("TWILIO_API_KEY_SECRET")
+	)
+
+	twilioClient := twilio_client.Client{
+		Credentials: twilio_client.NewCredentials(
+			twilioAPIKey,
+			twilioAPIKeySecret,
+		),
+	}
+	twilioClient.SetAccountSid(twilioAccount)
+
+	client := twilio.NewRestClientWithParams(
+		twilio.ClientParams{
+			Client: &twilioClient,
+		},
+	)
+
+	maxMessagesToList := 1000
+
+	keys, err := client.Api.ListKey(&twilio_api.ListKeyParams{
+		Limit: &maxMessagesToList,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, k := range keys {
+		if strings.HasPrefix(*k.FriendlyName, "vault-secrets") {
+			err := client.Api.DeleteKey(*k.Sid, &twilio_api.DeleteKeyParams{})
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println("deleted", *k.FriendlyName)
+			}
+		}
+		fmt.Println("found", *k.FriendlyName)
+	}
+}
